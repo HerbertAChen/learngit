2e3734564743615a9a26b5032a6e3765b36e7050
HerbertAchen <u8800cgq@gmail.com>
2016-12-24 16:30:12 +0800

    add some code

--- ex1.py
+++ ex1.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 print "Hello World!"
 print "Hello Again"
 print "I like typing this."
@@ -7,3 +8,5 @@ print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 print 'I "said" do not touch this.'
 print "why do it show this line only ?"
+print "I'm work at CU"
+print "我在国企工作"
