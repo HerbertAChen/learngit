47e05d2cad16235618e32c2794b9255634dd88b4
HerbertAchen <u8800cgq@gmail.com>
2016-12-25 20:06:32 +0800

    More Printing

--- ex7.py
+++ ex7.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
+print "Its fleece was white as %s." % 'snow' #使用%s代替后面的% ''的内容
 print "And everywhere that Mary went."
 print "." * 10 #Can print ten dots
 
@@ -20,8 +20,9 @@ end12="r"
 #watch that command at the end. try removing it to see what happens
 print end1+end2+end3+end4+end5+end6
 print end7+end8+end9+end10+end11+end12
+print end1 + end2 + end3 + end4 + end5 + end6, end7 + end8 + end9 + end10 + end11 + end12#
 
 print end1 * 10
-#print "Its fleece was white as %s." * 10  % 'snow'(错误是因为* 10的位置不对，如下面两条)
+#print "Its fleece was white as %s." * 10  % 'snow' #错误是因为* 10的位置不对，如下面两条:
 print "Its fleece was white as ." * 10
-print "Its fleece was white as %s."  % 'snow' * 10
+print "Its fleece was white as %s."  % 'snow' * 10 #放在后面才行。
