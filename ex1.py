f02802f7e08025c9437bafdeee07b07322ba761e
HerbertAchen <u8800cgq@gmail.com>
2016-12-25 20:02:01 +0800

    Exercise 1: A Good First Program

--- ex1.py
+++ ex1.py
@@ -1,12 +1,15 @@
 #-*- coding: utf-8 -*-
-print "Hello World!"
+print "Hello World!" #打印出一个字符串
 print "Hello Again"
 print "I like typing this."
 print "This is fun."
-print 'Yay! Printing.'
+print 'Yay! Printing.'#使用单引号也是可以的
+print 'e'
+print "e"
+print "every"
+print 'every'
 print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-print 'I "said" do not touch this.'
+print 'I "said" do not touch this.' #打印出两个单引号之间的内容，里面包含双引号也打印出来了
 print "why do it show this line only ?"
 print "I'm work at CU"
-#print "我在国企工作."
+#print "我在国企工作." #现在还不知道如何打印出中文文，应该输出工具的设置问题，后面再研究，已在issue中记录。
