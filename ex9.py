f9f62e0f36409fa852ba945efc9f0802c7489887
HerbertAchen <u8800cgq@gmail.com>
2016-12-25 21:37:34 +0800

    Exercise 9: Printing, Printing, Printing

--- ex9.py
+++ ex9.py
@@ -12,6 +12,6 @@ With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
-#这里的3个引号对是不能增加和删除的。
+#这里的3个引号对是不能增加和删除的。You have to type them like """ and not " " ", meaning with no spaces between each one.
 
 print days , " %r \n"  % months #That's how %r formatting works; it prints it the way you wrote it (or close to it). It's the "raw" format for debugging.
